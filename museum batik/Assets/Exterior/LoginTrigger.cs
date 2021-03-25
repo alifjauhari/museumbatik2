ba6f53007098ef9a612fbea67f3f898eeadc6cd4
Syamsul <syamsulnizar.sn@gmail.com>
2021-03-25 13:36:28 +0700

    Tinggal dikit lagi

---
+++ museum batik/Assets/Exterior/LoginTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginTrigger : MonoBehaviour
+{
+
+    public GameObject trigger;
+ 
+ void Start () {
+         trigger.SetActive (false);
+     }
+ 
+ void OnTriggerEnter(Collider _col){
+         if (_col.gameObject.CompareTag ("Login")) {
+             trigger.SetActive (true);
+                 }
+         }
+
+         void OnTriggerExit(Collider _col){
+         if (_col.gameObject.CompareTag ("Login")) {
+             trigger.SetActive (false);
+         }
+     }
+}
---
+++ museum batik/Assets/Exterior/game.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class game : MonoBehaviour
+{
+    public Text playerDisplay;
+    public Text scoreDisplay;
+
+    private void Awake()
+    {
+        if (DBManager.playername == null)
+        {
+            SceneManager.LoadScene(0);
+        }
+        playerDisplay.text = "Player: " + DBManager.playername;
+        scoreDisplay.text = "Score: " + DBManager.highscore;
+    }
+
+    public void CallSaveData()
+    {
+        StartCoroutine(SavePlayerData());
+    }
+
+    IEnumerator SavePlayerData()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", DBManager.playername);
+        form.AddField("score", DBManager.highscore);
+
+        WWW www = new WWW("http://localhost/try/savedata.php", form);
+        yield return www;
+        if (www.text == "0")
+        {
+            Debug.Log("Game Saved.");
+        }
+        else
+        {
+            Debug.Log("Save failed. Error#" + www.text);
+        }
+
+        DBManager.LogOut();
+        SceneManager.LoadScene(0);
+    }
+
+
+    //u can use this void if you dont want log out but wanna exit the game (it wasnt used automatically)
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void IncreaseScore()
+    {
+        DBManager.highscore++;
+        scoreDisplay.text = "Score: " + DBManager.highscore;
+    }
+}
